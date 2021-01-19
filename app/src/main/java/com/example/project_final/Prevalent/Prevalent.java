47018957f60595bed5f951f10ee0eff213641e6a
Salman <salman.hanif524@gmil.com>
2021-01-19 20:19:45 +0500

    Hello Zeeshan

---
+++ app/src/main/java/com/example/project_final/Prevalent/Prevalent.java
@@ -0,0 +1,14 @@
+package com.example.project_final.Prevalent;
+
+import com.example.project_final.Model.Users;
+
+public class Prevalent
+{
+   Prevalent(){
+
+    }
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
